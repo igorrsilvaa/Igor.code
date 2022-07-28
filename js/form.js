3721d8a1921c977614f79dd33e148cb9333a451a
igorrsilvaa <igorrsilvaa920@gmail.com>
2022-07-27 21:02:32 -0300

    Melhorias

---
+++ js/form.js
@@ -0,0 +1,9 @@
+const init = () => {
+    const inputName = document.querySelector('input[name="UserName"]');
+    const inputEmail = document.querySelector('input[name="UserEmail"]');
+    const inputObject = document.querySelector('input[name="Object"]');
+    const inputMessage = document.querySelector('input[name="Message"]');
+    const submitButton = document.querySelector('.submitform');
+
+    console.log('inputName, inputEmail, inputObject, inputMessage, submitButton');
+}
