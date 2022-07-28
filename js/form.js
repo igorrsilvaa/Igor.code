a022acb91cda7a690f533b34eb968a734da7a28e
igorrsilvaa <igorrsilvaa920@gmail.com>
2022-07-27 22:15:58 -0300

    Validacao_do_Fomulario

--- js/form.js
+++ js/form.js
@@ -1,9 +1,6 @@
-const init = () => {
-    const inputName = document.querySelector('input[name="UserName"]');
-    const inputEmail = document.querySelector('input[name="UserEmail"]');
-    const inputObject = document.querySelector('input[name="Object"]');
-    const inputMessage = document.querySelector('input[name="Message"]');
-    const submitButton = document.querySelector('.submitform');
-
-    console.log('inputName, inputEmail, inputObject, inputMessage, submitButton');
+function ChamaAlert(){
+    var Alert
+    if(Alert != ""){
+        alert("Email enviado com sucesso!");
+    }   
 }
